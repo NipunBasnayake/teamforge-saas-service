3e32c9232d6485bcd216f4a0ae0f81e31505f60e
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-12-05 00:53:14 +0530

    Initialize server

---
+++ teamforge-service/src/main/java/com/teamforge/TeamforgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teamforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamforgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamforgeServiceApplication.class, args);
+	}
+
+}
---
+++ teamforge-service/src/test/java/com/teamforge/TeamforgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamforgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
