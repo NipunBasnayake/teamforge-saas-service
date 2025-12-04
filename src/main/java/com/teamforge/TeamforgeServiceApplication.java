b91db3f2f13e77b64471d1ad2f90af979981641e
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-12-05 03:02:40 +0530

    Add environment configuration and update application properties

--- src/main/java/com/teamforge/TeamforgeServiceApplication.java
+++ src/main/java/com/teamforge/TeamforgeServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeamforgeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeamforgeServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TeamforgeServiceApplication.class, args);
+    }
 }
