3834b4f399041887496371dfded4e769cde05e40
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-12-05 09:56:17 +0530

    Refactor application configuration to use environment variables and correct class name casing

--- src/main/java/com/teamforge/TeamForgeServiceApplication.java
+++ src/main/java/com/teamforge/TeamForgeServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeamforgeServiceApplication {
+public class TeamForgeServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TeamforgeServiceApplication.class, args);
+        SpringApplication.run(TeamForgeServiceApplication.class, args);
     }
 }
